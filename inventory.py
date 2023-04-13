e58ac5129adc045b36b8706d3a91050f240f3c3b
molpako <system.222.down@gmail.com>
2023-04-13 14:23:47 +0900

    fix

--- inventory.py
+++ inventory.py
@@ -1,7 +1,9 @@
-#!venv/bin/python
+from pathlib import Path
 import json
 import subprocess
 import sys
+import os
+from collections import defaultdict
 
 # multipassのVMのリストを取得するコマンド
 cmd = ['multipass', 'list', '--format', 'json']
@@ -9,28 +11,50 @@ result = subprocess.run(cmd, stdout=subprocess.PIPE)
 
 # multipassコマンドの出力をJSONデータに変換する
 instances = json.loads(result.stdout.decode('utf-8'))
-names = [instance["name"] for instance in instances["list"]]
+inventory = {
+    "all": {
+        "vars": {
+            'ansible_port': 22,
+            "ansible_ssh_user": "ubuntu",
+            'ansible_ssh_private_key_file': str(Path(os.path.dirname(__file__))/"multipass.key"),
+        }
+    },
+    "clients": {
+        "children": ["mons", "mgrs", "osds"],
+    },
+    "mons": {
+        "hosts": [],
+    },
+    "osds": {
+        "hosts": [],
+    },
+    "admin": {
+        "hosts": [],
+    },
+    "_meta": {
+        "hostvars": defaultdict(dict),
+    },
+}
 
-# Ansibleに渡すためのホストリストを作成する
-host_vars = {}
+_meta_vars = inventory['_meta']['hostvars']
 for instance in instances["list"]:
-    host_vars[instance["name"]] = {
-        'ansible_host': instance['ipv4'][0],
-        'ansible_port': 22,
-        'ansible_user': 'ubuntu',
-        'ansible_ssh_private_key_file': './multipass.key'
-    }
+    name = instance["name"]
+    key = f"{name[:3]}s"
+    if name == "mon1":
+        key = "admin"
+    inventory[key]["hosts"].append(name)
+    _meta_vars[name]["ansible_host"] = instance['ipv4'][0]
 
-# Ansibleが理解できる形式でJSONを出力する
-if len(sys.argv) == 2 and sys.argv[1] == '--list':
+# Write by ChatGPT-3
+if len(sys.argv) == 2 and sys.argv[1] == '--list' or len(sys.argv) < 2:
     # --listフラグが渡された場合、ホストリスト全体を出力する
-    print(json.dumps({'_meta': {'hostvars': host_vars}, 'all': {'hosts': names}}))
+    print(json.dumps(inventory))
 elif len(sys.argv) == 3 and sys.argv[1] == '--host':
     # --hostフラグが渡された場合、指定されたホストの情報を出力する
     hostname = sys.argv[2]
-    host = next((name for name in names if name == hostname), None)
+    host = next((hostname for instance in instances["list"] if instance["name"] == hostname), None)
     if host:
-        print(json.dumps({'_meta': {'hostvars': {}}, 'host': host}))
+        print(json.dumps({'_meta': {'hostvars': _meta_vars[host]}, 'host': host}))
     else:
         print(json.dumps({'_meta': {'hostvars': {}}, 'host': {}}))
 else:
